542c3a97b5070712537381011611939bf8586d0c
Yaroslav Shevchenko <yaroslav.shevchenko@globallogic.com>
2019-01-04 18:40:03 +0200

    initial commit

---
+++ src/main/java/com/example/cloudclientapp/CloudClientAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.cloudclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudClientAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudClientAppApplication.class, args);
+    }
+
+}
+
